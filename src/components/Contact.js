45bd04a07a9fbedf48c370ab31bd408e8b4f6ff2
Susanpaudel <susanpaudel15@gmail.com>
2023-01-02 21:54:03 +0545

    upload

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,38 @@
-import React from 'react'
+import React,{ useRef } from 'react'
 import { useState } from 'react'
+import emailjs from 'emailjs-com'
 
 export const Contact = () => {
+    const form = useRef();
   const[name, setName] = useState('');
   const[email, setEmail] = useState('');
   const[message, setMessage] = useState('');
-  
   let submit=(e)=>{
    e.preventDefault();
    if(!name){
     alert("Please enter your name")
    }else if(!email){
     alert("Please enter your email")
+   }else if(email==='susanpaudel15@gmail.com' || email==='SUSANPAUDEL15@GMAIL.COM'){
+    alert("This is my email address. Please enter your valid email address.")
    }else if(!message){
     alert("Please enter your message")
    }else{
-    alert('Thanks for your message!');
-    setName('');
-    setEmail('');
-    setMessage('');
+    emailjs.sendForm('service_cywmlsj', 'template_j2jvp1f', form.current, 'vxnXAj8KlU7JJ8dEX')
+        .then(() => {
+            alert("Your mail send successfully!");
+            setName('');
+            setEmail('');
+            setMessage('');
+        }, () => {
+            alert("Something went wrong!Please try again letter.");
+        }
+        );
+    
+  
    }
   }
+
   return (
     <div className="container">
         <div className="row">
@@ -42,14 +54,14 @@ export const Contact = () => {
              </div>
             </div>
             <div className="col-lg-7">
-            <form className="htmlForm my-3" onSubmit={submit }>
+            <form className="htmlForm my-3" onSubmit={submit } ref={form}>
                 <p className='text-secondary text-uppercase'>TELL US ABOUT YOUR PROJECT AND GOALS.</p>
                     <div className="mb-3 input-field">
-                        <input type="text" id="name" className="text-box" placeholder=" " value={name} onChange={(e)=>{setName(e.target.value)}}/>
+                        <input type="text" id="name" name="name" className="text-box" placeholder=" " value={name} onChange={(e)=>{setName(e.target.value)}}/>
                         <label htmlFor="name" className="htmlForm-label text-secondary ps-3">Full Name</label><span className="icon_htmlForm_reg"><i className="fa fa-user" aria-hidden="true"></i></span>
                     </div>
                     <div className="mb-3 input-field">
-                        <input type="email" id="email" className="text-box" placeholder=" " value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+                        <input type="email" id="email" name='email' className="text-box" placeholder=" " value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
                         <label htmlFor="email" className="htmlForm-label text-secondary ps-3">Email</label><span className="icon_htmlForm_reg"><i className="fa fa-envelope" aria-hidden="true"></i></span>
                     </div>
                     <div className="mb-3 input-field">
